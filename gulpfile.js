2e4a53900ec1305c750c045831c0bd0f7b13bfcf
Bruna Martins <bruninha.iub@gmail.com>
2023-05-30 08:10:13 -0300

    ultimas alterações

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require ('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require ('gulp-imagemin');
 
+
+
 function styles() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({ outputStyle:'compressed' }))
@@ -18,4 +20,5 @@ exports.default = gulp.parallel(styles, images);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    
 }
